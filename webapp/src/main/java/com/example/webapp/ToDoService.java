35f5e60a4e00d558d934b3851f587a52477d3f23
Cipher-implosion <bua80vo@gmail.com>
2026-01-25 23:26:15 +0900

    インターフェース「Service」を作成

---
+++ webapp/src/main/java/com/example/webapp/ToDoService.java
@@ -0,0 +1,36 @@
+package com.example.webapp;
+
+import java.util.List;
+
+import com.example.webapp.entity.ToDo;
+
+/**
+ * ToDo : サービス
+ */
+public interface ToDoService {
+	
+	/**
+	 * 全「すること」を検索します。 
+	 */
+	List<ToDo> findAllToDo();
+	
+	/**
+	 * 指定されたIDの「すること」を検索します。 
+	 */
+	ToDo findByIdToDo(Integer Id);
+	
+	/**
+	 * 「すること」を新規登録します。 
+	 */
+	void insertToDo(ToDo toDo);
+	
+	/**
+	 * 「すること」を更新します。 
+	 */
+	void updateToDo(ToDo toDo);
+	
+	/**
+	 * 「すること」を削除します。 
+	 */
+	void deleteToDo(ToDo toDo);
+}
