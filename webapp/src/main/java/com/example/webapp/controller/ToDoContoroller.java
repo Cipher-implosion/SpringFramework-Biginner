c42352873aec155762cc2941eb1684e2f91e229a
Cipher-implosion <bua80vo@gmail.com>
2026-01-26 21:53:53 +0900

    Controllerの作成

---
+++ webapp/src/main/java/com/example/webapp/controller/ToDoContoroller.java
@@ -0,0 +1,54 @@
+package com.example.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.webapp.Service.ToDoService;
+import com.example.webapp.entity.ToDo;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * ToDoコントローラ
+ */
+@Controller
+@RequestMapping("/todos")
+@RequiredArgsConstructor
+public class ToDoContoroller {
+	
+	/** DI */
+	private final ToDoService toDoService;
+	
+	/**
+	 * 「すること」の一覧を表示します。
+	 */
+	@GetMapping
+	public String kist(Model model) {
+		model.addAttribute("todos", toDoService.findAllToDo());
+		return "todo/list";
+	}
+	
+	/**
+	 * 指定されたIDの「すること」の詳細を表示します。
+	 */
+	@GetMapping("/{id}")
+	public String detail(@PathVariable Integer id, Model model, 
+			RedirectAttributes attributes) {
+		// 「すること」IDに対応する「すること」情報を取得
+		ToDo toDo = toDoService.findByIdToDo(id);
+		if (toDo != null) {
+			// 対象データがある場合はモデルに格納
+			model.addAttribute("todo", toDo);
+			return "todo/detail";
+		} else {
+			// 対象データがない場合はフラッシュメッセージを設定
+			attributes.addFlashAttribute("errorMessage", "対象データがありません");
+			// リダイレクト
+			return "redirect;/todos";
+		}
+	}
+}
